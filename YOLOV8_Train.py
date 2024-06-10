81d32ec3571e7309bdb78906d7acf44758136a7e
egan-puno <francischuegan@yahoo.com.ph>
2024-06-10 21:21:30 +0800

    Update YOLOV8_Train.py

--- YOLOV8_Train.py
+++ YOLOV8_Train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n-seg.pt')
 
-model.train(data='/content/gdrive/My Drive/PUNO_NG_PAGMAMAHAL/TASK_FILES/YOLOV8_Colab/config.yaml', epochs = 100, imgsz = 320, batch = -1, device=0, plots = False)
+model.train(data='directory/config.yaml', epochs = 100, imgsz = 320, batch = -1, device=0, plots = False)
